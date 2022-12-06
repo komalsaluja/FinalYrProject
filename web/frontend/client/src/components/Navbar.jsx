35c71805880865db6a29d99abeb86ba243d12c70
komalsaluja <ks5908491@gmail.com>
2022-12-07 00:11:32 +0530

    navbar

---
+++ web/frontend/client/src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function BasicExample() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home" style={{fontSize:'3rem'}}>Sanjivani-Hospital</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Services</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
+            <Nav.Link href="#home">About</Nav.Link>
+           
+          </Nav>
+
+          <Nav className="mb-auto">
+            <Nav.Link href="#home">SignUp</Nav.Link>
+            <Nav.Link href="#link">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
